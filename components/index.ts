16bbc634b5ffd37108361d5abd089961eef281df
daviddkkim <david.kim@richmond.edu>
2022-12-06 11:40:24 -0500

    initialize the project with stitches and radix set up

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+import Layout from "./layout/main";
+import { Box } from "./Box";
+
+export {
+    Layout,
+    Box
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,78 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ThemeProvider } from "next-themes";
+import { darkTheme, globalCss } from "../stitches.config";
+import type { AppProps } from "next/app";
+import { ReactElement, ReactNode, useEffect, useState } from "react";
+import { NextPage } from "next";
+import React from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const globalStyles = globalCss({
+  html: {
+    overflowX: "hidden",
+  },
+
+  body: {
+    margin: 0,
+    backgroundColor: "$mauve1",
+    color: "$mauve12",
+  },
+
+  "body, button": {
+    fontFamily: "$untitled",
+  },
+
+  svg: { display: "block" },
+
+  "pre, code": { margin: 0, fontFamily: "$mono" },
+
+  "::selection": {
+    backgroundColor: "$mint11",
+    color: "$sage1",
+  },
+  "*": {
+    boxSizing: "border-box",
+  },
+  h1: {
+    fontSize: "$6",
+    fontWeight: 500,
+  },
+  "h2, h3": {
+    fontSize: "$5",
+    fontWeight: 500,
+  },
+});
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  globalStyles();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return (
+    <ThemeProvider
+      disableTransitionOnChange
+      attribute="class"
+      value={{ light: "light-theme", dark: darkTheme.className }}
+      defaultTheme="dark"
+    >
+          {mounted ? (
+            getLayout(<Component {...pageProps} />)
+          ) : (
+            <div style={{ visibility: "hidden" }}>
+              <Component {...pageProps} />
+            </div>
+          )}
+    </ThemeProvider>
+  );
 }
-
-export default MyApp
